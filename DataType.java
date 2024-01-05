5b66b19910857d6ad28bb0a1e08e55dcc972656d
5ujit <sksujit021@gmail.com>
2024-01-05 13:26:40 +0530

    java basics

---
+++ DataType.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        int num  =1;
+        byte by  = 23;
+        short sh = 29333;
+        long l = 299999999;
+
+        float f = 5.2f;
+
+
+
+    }
+
+}
