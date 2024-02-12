ff7729512c70683b782fdd7f0177d60ecfc03198
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-12 19:54:29 +0530

    AbsoluteValue.

---
+++ Ninja_java/src/conditionals_if_else/AbsoluteValue.java
@@ -0,0 +1,19 @@
+package conditionals_if_else;
+import java.util.Scanner;
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        System.out.println("Enter the number ");
+
+        int num = ab.nextInt();
+        if(num <0){// if num is less then zero
+            System.out.println(num * -1);
+
+        }
+        else {
+            System.out.println(num * -1);
+
+        }
+
+    }
+}
