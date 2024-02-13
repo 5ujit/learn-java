0772bf172178359154c7243f9f586609430842b6
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-13 23:45:07 +0530

    while

---
+++ Ninja_java/src/loops/WhileLoops.java
@@ -0,0 +1,16 @@
+package loops;
+//import java.util.Scanner;
+public class WhileLoops {
+    public static void main(String[] args) {
+//        Scanner wl = new Scanner(System.in);
+//        int n = wl.nextInt();
+
+//        int num = 1;
+    int i = 0;
+    while (i < 5){
+        System.out.println(i);
+        i++;
+    }
+    }
+
+}
