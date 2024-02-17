69c78e291ed2899ad83ed01c3aac09b1f99c514c
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-17 23:59:10 +0530

    v

---
+++ Ninja_java/src/ProblemsForLoops/CountDigits.java
@@ -0,0 +1,20 @@
+package ProblemsForLoops;
+import java.util.Scanner;
+public class CountDigits {
+    public static void main(String[] args) {
+
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+	int numOfDigits = 0;
+	int original_n = n;
+	while(n>0){
+		n = n/10;
+		numOfDigits++;//numOfDigits
+		
+	}
+        System.out.println("Nu");
+
+    }
+
+}
