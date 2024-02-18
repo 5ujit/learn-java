8ecaa43d24e4b5433ccbb076779c758f90bb32dd
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-18 23:03:59 +0530

    sum  of digit

--- Ninja_java/src/ProblemsForLoops/SumOfDigits.java
+++ Ninja_java/src/ProblemsForLoops/SumOfDigits.java
@@ -1,4 +1,19 @@
 package ProblemsForLoops;
+import java.util.Scanner;
 
 public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sumOfDigits = 0;
+        int originalNumber = n; // Store the original number
+
+        while (n > 0) {
+            int digit = n % 10;
+            sumOfDigits += digit;
+            n = n / 10;
+        }
+        System.out.println("Sum of digits in " + originalNumber + " = " + sumOfDigits);
+    }
 }
