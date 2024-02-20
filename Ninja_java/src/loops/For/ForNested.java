cc69be285820a469501bd3a03224296f074a6b7a
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-20 22:40:31 +0530

    inhertance

---
+++ Ninja_java/src/loops/For/ForNested.java
@@ -0,0 +1,16 @@
+package loops.For;
+
+public class ForNested {
+    public static void main(String[]args){
+        // outer loop
+        for (int i=1; i<=2; i++){
+            System.out.println("Outer"+ i);//execute 2 times
+            //Inner loop
+            for (int j=1; j<=3; j++){
+                System.out.println("inner :"+ j);
+
+            }
+        }
+
+    }
+}
