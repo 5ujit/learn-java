5c566b0cf0bd2a6c44a599a6fb0d21c6a6129608
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-16 17:25:45 +0530

    Create First.java

---
+++ Ninja_java/src/Quesforlogics/First.java
@@ -0,0 +1,15 @@
+// check the integer is even or odd ??
+import java.util.*;
+public class First{
+  public static void main(String []a){
+    Scanner oe= new Scanner(System.in);
+
+    System.out.println("Enter a number");
+
+    int num = oe.nextInt();
+
+    System.out.println(num);
+
+
+  }
+}
