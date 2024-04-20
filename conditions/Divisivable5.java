a743247ecd4507856fc8ecdafe50d5a4a18ce5f9
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-20 13:00:31 +0530

    practic

---
+++ conditions/Divisivable5.java
@@ -0,0 +1,14 @@
+package conditions;
+import java.util.Scanner;
+
+
+public class Divisivable5{
+  public static void main(String []args){
+    
+
+
+
+  }
+
+}
+  
---
+++ conditions/EvenOdd.java
@@ -0,0 +1,22 @@
+package conditions;
+
+/**
+ * EvenOdd
+ */
+import java.util.Scanner;
+public class EvenOdd {
+  public static void main(String[] args) {
+    Scanner ab =  new Scanner (System.in);
+     
+    System.out.println("Enter a number for input");
+    int n = ab.nextInt();
+
+    System.out.println(n);
+
+  
+
+
+  }
+
+  
+}
