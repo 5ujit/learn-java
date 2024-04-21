af1f0e27df21b9fb1a783e89ea60c4b25b207bad
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-21 23:36:32 +0530

    Create EvenOdd.java

---
+++ Ninja_java/src/Quesforlogics/EvenOdd.java
@@ -0,0 +1,16 @@
+/**
+ * EvenOdd
+ */
+
+
+ import java.util.Scanner;
+public class EvenOdd {
+  public static void main(String[] args) {
+    Scanner ab =  new Scanner(System.in);
+     
+    System.out.println("Enter a number for input:");
+    int n = ab.nextInt();
+
+    if(n% 2 == 0);
+  }
+}
