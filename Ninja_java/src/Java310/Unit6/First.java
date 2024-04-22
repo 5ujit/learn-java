7a0502d001e1288961176bfd26d09e05d54e627c
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-22 19:59:12 +0530

    unit 6

---
+++ Ninja_java/src/Java310/Unit6/First.java
@@ -0,0 +1,23 @@
+// package Map;
+import java.util.*;
+public class First {
+  public static void main(String[] args) {
+    Map<String, String> map= new LinkedHashMap<>();
+    map.put("123", "amit");
+    map.put("111", "k;afdk Shmit");
+    map.put("123", "k;Steve Yao" );
+    map.put("222", "k;Steve Yao");
+
+    System.out.println(map.get("111"));
+    map.clear();
+
+    System.out.println("Size: " + map.size());
+
+
+
+    
+
+    }
+    
+  }
+  
