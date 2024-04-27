7808442f981b4bf7e760fa5cfaea9dc1f202ebf4
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-27 23:54:35 +0530

    Create Maps1.java

---
+++ Ninja_java/src/310/class22-4-24/Map/Maps1.java
@@ -0,0 +1,18 @@
+package Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Maps1 {
+  public static void main(String[] args) {
+    Map<String,ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+    
+    map.put("Ford",new ArrayList<String>());
+    map.get("Ford").add(("Gt"));
+
+    
+
+  }
+  
+}
