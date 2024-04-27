57a68dd14080ce587c74f8b6d46396c67c26f6eb
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-27 23:53:48 +0530

    Create Question.java

---
+++ Ninja_java/src/310/class16-4-24/Question.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+public class Question {
+  public static void main(String[] args) {
+    ArrayList<String> Ac = new ArrayList<>();
+
+     Scanner  sc = new Scanner(System.in);
+     boolean  b= true;
+     while (sc.hasNext()) {
+      String a= sc.next();
+      if (!a.matches("[a-zA-Z+]")) {
+        
+        
+      }
+      
+     }
+
+    
+  }
+
+  
+}
