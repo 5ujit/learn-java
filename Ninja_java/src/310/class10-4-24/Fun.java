b5a8cf960de38054499142037d36ca0d10b5a273
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-27 23:53:41 +0530

    Create Fun.java

---
+++ Ninja_java/src/310/class10-4-24/Fun.java
@@ -0,0 +1,16 @@
+class fun <T>{
+
+ T a;
+ void fun1(Tb){
+  a= b;4;
+  System.out.println(a);
+ }}
+ Te{
+  public static void main(String [] args){
+    fun<Integer >obj= new fun <Strung>();
+    fun<Integer >obj1= new fun <Strung>();
+    obj.fun1(5,"Mukesh");
+
+    obj1.fun1("Mukesh", 6)
+  }
+ }
