a53b64637d4daa90fd05c6d0fdfc96375b583966
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-27 23:55:01 +0530

    Create Main.java

---
+++ Ninja_java/src/310/class23-3-24/abcd/Main.java
@@ -0,0 +1,10 @@
+package abcd;
+import java.time. *;
+public class Main {
+    public static void main(String[] args) {
+		LocalTime obj = Local.of(14,30,90);
+		obj= obj.WithDayOfYear(90);
+
+    }
+    
+}
