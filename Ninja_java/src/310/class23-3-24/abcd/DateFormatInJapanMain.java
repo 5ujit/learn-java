e25335c6bf1a611e15a906df67719a0f5161a739
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-27 23:54:59 +0530

    Create DateFormatInJapanMain.java

---
+++ Ninja_java/src/310/class23-3-24/abcd/DateFormatInJapanMain.java
@@ -0,0 +1,17 @@
+package abcd;
+
+import java.util.Calendar;
+
+public class DateFormatInJapanMain {
+    public static void main(String[] args) {
+//        Calendar c =
+//        Calendar.getInstance();
+//        c.set(Integer.parseInt(args[0]),
+//                Integer.parseInt((args[1])..1,
+//        Integer.parseInt(args[2]),10,00,);
+//        System.out.println(DateForamt.getDateInstance().format(c.getTime()));
+//        )
+
+
+    }
+}
