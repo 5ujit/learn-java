8c2acfa843c5182152ad203e0f070eba81e892ed
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-29 23:53:46 +0530

    Create Task.java

---
+++ Ninja_java/src/310/class8-4-24/Task.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Task{
+  public static void main(String [] args){
+    try{
+      FileHandler fl= new FileHandler('sk.txt');
+      BufferReader br = new BufferReader(fs);
+      String data= ""
+      while (data = br.readLine())
+
+    }
+     
+  }
+
+}
