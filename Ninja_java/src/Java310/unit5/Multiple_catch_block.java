fb149e8e74d592d182b7d1129575af03f73a0162
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-29 23:53:39 +0530

    Update Multiple_catch_block.java

--- Ninja_java/src/Java310/unit5/Multiple_catch_block.java
+++ Ninja_java/src/Java310/unit5/Multiple_catch_block.java
@@ -2,6 +2,19 @@ public class Multiple_catch_block {
    
 
 public static void main(String[] args) {
+  try {
+
+    int a=10, b=0,c;
+    c= a/b;
+    System.out.println(c);
+
+
+  }
+
+  catch (Exception x){
+    System.out.println("handling all type of exception ");
+
+  }
   
 }
   
