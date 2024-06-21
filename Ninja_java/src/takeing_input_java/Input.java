60264f8c97cdbd953e67913a96ec97229c8050e1
5ujit <149666757+5ujit@users.noreply.github.com>
2024-06-21 18:15:30 +0530

    Create Input.java

---
+++ Ninja_java/src/takeing_input_java/Input.java
@@ -0,0 +1,24 @@
+// inputs in java
+package takeing_input_java;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+//
+        Scanner b = new Scanner (System.in);
+//
+        System.out.println("Enter you name");
+        String name = b.next();
+        System.out.println("this is your name "+ name);
+
+
+        System.out.println("enter your lucky number");
+        int num1 = b.nextInt();
+
+        System.out.println("this is your lucky num  " +num1);
+        System.out.println("enter your name ");
+
+
+
+
+    }
+}
