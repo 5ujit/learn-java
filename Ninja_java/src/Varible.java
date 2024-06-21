0b12a0a72e52ef076d1511df6081a30c1cb7c3b4
5ujit <149666757+5ujit@users.noreply.github.com>
2024-06-21 18:15:50 +0530

    Create Varible.java

---
+++ Ninja_java/src/Varible.java
@@ -0,0 +1,22 @@
+public class Varible {
+    public static void main(String[] args) {
+        System.out.println("hello this is developer sujit");
+
+        boolean isPassed = true;
+        int count = 13;
+        byte marks = 127;
+
+        System.out.println(isPassed);
+        System.out.println(count);
+        System.out.println(marks);
+        double secondpi = 3.14;
+        float flo = 2.2f;
+
+        char myLetter = 'A';
+
+        System.out.println(myLetter);
+        System.out.println(flo);
+
+    }
+
+}
