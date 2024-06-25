b51c0ec687c641d069dbb6e1ba39442891c04dd2
5ujit <149666757+5ujit@users.noreply.github.com>
2024-06-25 17:34:32 +0530

    Create LargerAge.java

---
+++ Ninja_java/src/Condition/LargerAge.java
@@ -0,0 +1,8 @@
+package Condition;
+import  java.util.Scanner;
+public class LargerAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a
+    }
+}
